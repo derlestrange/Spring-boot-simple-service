7f3a1569ee7bc380c90d1670bafe3e707b1b7df1
derlestrange <56831326+derlestrange@users.noreply.github.com>
2021-01-21 21:38:46 +0400

    add service

---
+++ demo/src/main/java/com/example/demo/service/ToDoService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.model.ToDo;
+
+import java.util.List;
+
+public interface ToDoService {
+    public List<ToDo> getAllToDo();
+    public ToDo getToDoById(long id);
+    public ToDo saveToDo(ToDo todo);
+    public void removeToDo(ToDo todo);
+}
---
+++ demo/src/main/java/com/example/demo/service/ToDoServiceImplementation.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import com.example.demo.model.ToDo;
+import com.example.demo.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("toDoService")
+public class ToDoServiceImplementation implements ToDoService{
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @Override
+    public List<ToDo> getAllToDo() {
+        return toDoRepository.findAll();
+    }
+
+    @Override
+    public ToDo getToDoById(long id) {
+        return toDoRepository.getOne(id);
+    }
+
+    @Override
+    public ToDo saveToDo(ToDo todo) {
+        return toDoRepository.save(todo);
+    }
+
+    @Override
+    public void removeToDo(ToDo todo) {
+        toDoRepository.delete(todo);
+    }
+}
